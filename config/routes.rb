66b7382b902050db99bffafc8c7410ab04310e6c
jonathandreyer <jonathan.dreyer60@gmail.com>
2022-10-25 15:57:22 +0200

    Updated Tasks

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'about', to: 'pages#about'
+
+   #  url path    controller#action
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get "tasks/new", to: "tasks#new"
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
